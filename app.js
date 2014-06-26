47c2e16296bdc96e5afb6142085b98e3ad16cbce
Mudit Ameta <zeusdeux@gmail.com>
2014-06-26 12:39:21 +0530

    refactor

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
 var http = require('http');
-var tokenBoy = require('./token-boy');
+var url = require('url');
 
-http.createServer(tokenBoy).listen(process.env.TOKEN_BOY_PORT || 3001);
+var tokenBoy = require('./token-boy');
+var errBoy = require('./error-boy');
+
+function parseURL(req) {
+  var parsedUrl = url.parse(req.url, true);
+  for (var k in parsedUrl)
+    if (parsedUrl.hasOwnProperty(k))
+      req[k] = parsedUrl[k];
+  return true;
+}
+
+function listener(req, res) {
+  var token;
+  try {
+    parseURL(req);
+    if (req.method === 'GET') {
+      if (req.pathname === '/' && typeof parseInt(req.query.bits, 10) === 'number') {
+        token = tokenBoy(req.query.bits, req.query.digest);
+        res.writeHead(200, {
+          'Content-Length': token.length,
+          'Content-Type': 'text/plain'
+        });
+        res.end(token);
+      }
+      else {
+        res.end(errBoy(null, '404', req, res));
+      }
+    }
+    else {
+      res.end(errBoy(null, '404', req, res));
+    }
+  }
+  catch (e) {
+    res.end(errBoy(e, '500', req, res));
+  }
+}
+
+
+http.createServer(listener).listen(process.env.TOKEN_BOY_PORT || 3001);
--- token-boy.js
+++ token-boy.js
@@ -1,73 +1,15 @@
 var crypto = require('crypto');
-var url = require('url');
 
-function parseURL(req) {
-  var parsedUrl = url.parse(req.url, true);
-  for (var k in parsedUrl)
-    if (parsedUrl.hasOwnProperty(k))
-      req[k] = parsedUrl[k];
-  return true;
-}
-
-function errBoy(e, status, req, res) {
-  var codes = {};
-  var headers = {};
-
-  codes['404'] = '404: /' + req.method + ' ' + req.url + ' not found.';
-  headers['404'] = function() {
-    res.writeHead(404, {
-      'Content-Length': codes[status].length,
-      'Content-Type': 'text/plain'
-    });
-  };
-
-  if (e) {
-    codes['500'] = '500: Internal Server Error \n' + e.stack;
-    headers['500'] = function() {
-      res.writeHead(500, {
-        'Content-Length': codes[status].length,
-        'Content-Type': 'text/plain'
-      });
-    };
-  }
-
-  headers[status]();
-  return codes[status];
-}
-
-module.exports = function(req, res) {
+module.exports = function(bits, digest) {
   var token;
+  var buf;
   try {
-    parseURL(req);
-    if (req.method === 'GET') {
-      if (req.pathname === '/' && typeof parseInt(req.query.bits, 10) === 'number') {
-        crypto.randomBytes(parseInt(req.query.bits, 10), function(err, buf) {
-          try {
-            if (err) throw err;
-            else {
-              if (req.query.digest === 'true' || req.query.digest === '1') token = crypto.createHmac('SHA1', buf.toString('base64')).update((new Date()).toString()).digest('base64');
-              else token = buf.toString('base64');
-            }
-            res.writeHead(200, {
-              'Content-Length': token.length,
-              'Content-Type': 'text/plain'
-            });
-            res.end(token);
-          }
-          catch (e) {
-            res.end(errBoy(e, '500', req, res));
-          }
-        });
-      }
-      else {
-        res.end(errBoy(null, '404', req, res));
-      }
-    }
-    else {
-      res.end(errBoy(null, '404', req, res));
-    }
+    buf = crypto.randomBytes(parseInt(bits, 10));
+    if (digest === 'true' || digest === '1') token = crypto.createHmac('SHA1', buf.toString('base64')).update((new Date()).toString()).digest('base64');
+    else token = buf.toString('base64');
   }
   catch (e) {
-    res.end(errBoy(e, '500', req, res));
+    throw e;
   }
+  return token;
 };
